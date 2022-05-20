543bb30a7ac899a8d56b922568a640bb232197a3
Jake Gully <chimpytk@gmail.com>
2022-05-20 18:17:31 +0100

    v2.0.0
    - Allow for multiple types of cookies
    - Add an options section
    - Allow for implicit consent by scrolling

--- readme.md
+++ readme.md
@@ -16,10 +16,9 @@ npm install --save maya-vue-cookie-notice
 
 # Usage
 
-First, you'll want to extend `CookieNotice.vue'.
+You'll want to extend `maya-vue-cookie-notice' and override whichever handle methods you need.
 
-```
-// MyCookieNotice.vue
+```vue
 <script>
 import CookieNotice from 'maya-vue-cookie-notice';
 
@@ -27,7 +26,7 @@ export default {
   extends: CookieNotice,
 
   methods: {
-    handleAccepted() {
+    handleAcceptedPreference() {
       if (document.querySelector('[src^="https://example.com/script.js"]')) {
         var script = document.createElement('script');
         script.type = 'text/javascript';
@@ -36,28 +35,121 @@ export default {
       }
     },
 
-    handleRejected () {
+    handleRejectedPreference () {
       document.getElementById('myThingToRemove').remove();
+    },
+
+    handleAcceptedStatistic() {
+      // Statistics cookies were accepted
+    },
+
+    handleRejectedStatistic() {
+      // Statistics cookies were rejected
+    },
+
+    handleAcceptedMarketing() {
+      // Marketing cookies were accepted
+    },
+
+    handleRejectedMarketing() {
+      // Marketing cookies were rejected
     },
   }
 }
 </script>
 ```
 
+Then include your extended version in your app:
+
+```js
+import CookieNotice from './my-vue-files/CookieNotice.vue';
+
+new Vue({
+  el: '#app',
+
+  components: {
+    CookieNotice,
+  },
+});
+```
+
+When the user scrolls down, that will be taken as implied consent **(your text in the default slot should reflect this)**.
+
+The user can then choose to accept all the cookies or use the options button to filter the cookies or reject all of them.
+
 # Props
 
+- preference-cookies
+- statistic-cookies
+- marketing-cookies
+- label-class
 - wrapper-class
 - btn-wrapper-class
 - accept-btn-class
 - accept-btn-text
 - reject-btn-class
 - reject-btn-text
+- options-btn-class
+- options-btn-text
+- save-btn-class
+- save-btn-text
 - cookie-name
 - reset-selector
-- body-accepted-class
-- body-rejected-class
+- body-preference-accepted-class
+- body-preference-rejected-class
+- body-statistic-accepted-class
+- body-statistic-rejected-class
+- body-marketing-accepted-class
+- body-marketing-rejected-class
 
 # Events
 
-- accept
-- reject
+The following will both be emited from the component and triggered as events on the `body` element.
+
+- preferencecookiesaccepted
+- preferencecookiesrejected
+- statisticcookiesaccepted
+- statisticcookiesrejected
+- marketingcookiesaccepted
+- marketingcookiesrejected
+
+# Slots
+
+You can use the following slots to provide extra text.
+
+## Default slot
+
+```html
+<cookie-notice>
+  We've got cookies
+</cookie-notice>
+```
+
+## Options Text
+
+```html
+<cookie-notice>
+  <template v-slot:options-text>
+    Please select and accept your tracking preferences:
+  </template>
+</cookie-notice>
+```
+
+## Descriptions
+
+```html
+<cookie-notice>
+  <template v-slot:required-cookies-description>
+    Cookies for basic website functionality. These can't be turned off.
+  </template>
+  <template v-slot:preference-cookies-description>
+    Cookies to store your preferences.
+  </template>
+  <template v-slot:statistics-cookies-description>
+    Cookies to track how our site is being used in order to improve our service.
+  </template>
+  <template v-slot:marketing-cookies-description>
+    Cookies used by us and our advertising partners to provide you with personalised advertising.
+  </template>
+</cookie-notice>
+```
--- src/CookieNotice.vue
+++ src/CookieNotice.vue
@@ -1,13 +1,55 @@
 <template>
   <div :class="wrapperClass" v-if="show">
-    <slot />
+    <div v-if="showOptions">
+      <div>
+        <slot name="options-text" />
+      </div>
+
+      <div>
+        <label :class="labelClass">
+          <input type="checkbox" checked disabled />
+          Required cookies
+        </label>
+        <slot name="required-cookies-description" />
+      </div>
+
+      <div v-if="preferenceCookies">
+        <label :class="labelClass">
+          <input type="checkbox" v-model="cookieValue.preferenceCookies" />
+          Allow preference cookies
+        </label>
+        <slot name="preference-cookies-description" />
+      </div>
+
+      <div v-if="statisticCookies">
+        <label :class="labelClass">
+          <input type="checkbox" v-model="cookieValue.statisticCookies" />
+          Allow statistics cookies
+        </label>
+        <slot name="statistics-cookies-description" />
+      </div>
+
+      <div v-if="marketingCookies">
+        <label :class="labelClass">
+          <input type="checkbox" v-model="cookieValue.marketingCookies" />
+          Allow marketing cookies
+        </label>
+        <slot name="marketing-cookies-description" />
+      </div>
+    </div>
+
+    <slot v-else />
 
     <div :class="btnWrapperClass">
-      <button type="button" @click.prevent="userAccept" :class="acceptBtnClass">
-        {{ acceptBtnText }}
+      <template v-if="showOptions">
+        <button @click.prevent="rejectAll" :class="rejectBtnClass">{{ rejectBtnText }}</button>
+        <button @click.prevent="saveOptions" :class="saveBtnClass">{{ saveBtnText }}</button>
+      </template>
+      <button v-else @click.prevent="showOptions = !showOptions" :class="optionsBtnClass">
+        {{ optionsBtnText }}
       </button>
-      <button type="button" @click.prevent="userRejects" :class="rejectBtnClass">
-        {{ rejectBtnText }}
+      <button type="button" @click.prevent="userAcceptAll" :class="acceptBtnClass">
+        {{ acceptBtnText }}
       </button>
     </div>
   </div>
@@ -20,21 +62,45 @@ export default {
   data() {
     return {
       show: false,
+      showOptions: false,
+      consentImplied: false,
+      cookieSet: false,
+      cookieValue: {
+        preferenceCookies: true,
+        statisticCookies: true,
+        marketingCookies: true,
+      },
     };
   },
 
   props: {
+    preferenceCookies: {
+      type: Boolean,
+      default: true,
+    },
+    statisticCookies: {
+      type: Boolean,
+      default: true,
+    },
+    marketingCookies: {
+      type: Boolean,
+      default: true,
+    },
+    labelClass: {
+      type: String,
+      default: 'font-bold',
+    },
     wrapperClass: {
       type: String,
-      default: 'fixed right-5 bottom-5 rounded bg-white shadow z-50 p-5 max-w-sm',
+      default: 'fixed right-5 bottom-5 rounded bg-white shadow z-50 p-5 max-w-sm text-black',
     },
     btnWrapperClass: {
       type: String,
-      default: 'grid grid-cols-2 gap-2.5',
+      default: 'flex gap-2.5 mt-3',
     },
     acceptBtnClass: {
       type: String,
-      default: 'rounded bg-black text-white px-5 py-2.5',
+      default: 'flex-1 rounded bg-black text-white px-5 py-2.5',
     },
     acceptBtnText: {
       type: String,
@@ -42,11 +108,27 @@ export default {
     },
     rejectBtnClass: {
       type: String,
-      default: 'rounded border-2 border-black px-5 py-2.5',
+      default: 'flex-1 rounded border-2 border-black px-5 py-2.5 text-black',
     },
     rejectBtnText: {
       type: String,
       default: 'Reject',
+    },
+    optionsBtnClass: {
+      type: String,
+      default: 'flex-1 rounded border-2 border-black px-5 py-2.5 text-black',
+    },
+    optionsBtnText: {
+      type: String,
+      default: 'Options',
+    },
+    saveBtnClass: {
+      type: String,
+      default: 'flex-1 rounded border-2 border-black px-5 py-2.5 text-black',
+    },
+    saveBtnText: {
+      type: String,
+      default: 'Save',
     },
     cookieName: {
       type: String,
@@ -56,18 +138,34 @@ export default {
       type: String,
       default: '.show-cookie-notice',
     },
-    bodyAcceptedClass: {
+    bodyPreferenceAcceptedClass: {
       type: String,
-      default: 'cookies-accepted',
+      default: 'preference-cookies-accepted',
     },
-    bodyRejectedClass: {
+    bodyPreferenceRejectedClass: {
       type: String,
-      default: 'cookies-rejected',
+      default: 'preference-cookies-rejected',
+    },
+    bodyStatisticAcceptedClass: {
+      type: String,
+      default: 'statistic-cookies-accepted',
+    },
+    bodyStatisticRejectedClass: {
+      type: String,
+      default: 'statistic-cookies-rejected',
+    },
+    bodyMarketingAcceptedClass: {
+      type: String,
+      default: 'marketing-cookies-accepted',
+    },
+    bodyMarketingRejectedClass: {
+      type: String,
+      default: 'marketing-cookies-rejected',
     },
   },
 
   created() {
-    const cookie = this.getCookie();
+    this.getCookie();
 
     document.body.addEventListener('click', (event) => {
       if (event.target.matches(this.resetSelector)) {
@@ -79,36 +177,98 @@ export default {
       }
     });
 
-    if (!cookie) {
+    if (!this.cookieSet) {
       this.showDialog();
+      window.addEventListener('scroll', this.implicitConsent);
       return;
     }
 
-    if (cookie === 'allow') {
-      this.handleAcceptedAll();
-    } else {
-      this.handleRejectedAll();
-    }
+    this.handleChoices();
   },
 
   methods: {
-    userAccept() {
-      this.setCookie('allow');
-      this.handleAcceptedAll();
+    implicitConsent() {
+      this.handleAcceptedPreferenceAll();
+      this.handleAcceptedStatisticAll();
+      this.handleAcceptedMarketingAll();
+      window.removeEventListener('scroll', this.implicitConsent);
+    },
+
+    handleChoices() {
+      if (this.cookieValue.preferenceCookies) {
+        this.handleAcceptedPreferenceAll();
+      } else {
+        this.handleRejectedPreferenceAll();
+      }
+
+      if (this.cookieValue.statisticCookies) {
+        this.handleAcceptedStatisticAll();
+      } else {
+        this.handleRejectedStatisticAll();
+      }
+
+      if (this.cookieValue.marketingCookies) {
+        this.handleAcceptedMarketingAll();
+      } else {
+        this.handleRejectedMarketingAll();
+      }
+    },
+
+    userAcceptAll() {
+      this.setCookie();
+      this.handleAcceptedPreferenceAll();
+      this.handleAcceptedStatisticAll();
+      this.handleAcceptedMarketingAll();
       this.hideDialog();
     },
 
-    handleAcceptedAll() {
-      document.body.classList.remove(this.bodyRejectedClass);
-      document.body.classList.add(this.bodyAcceptedClass);
-      this.handleAccepted();
-      this.$emit('accept');
-      const event = new CustomEvent('cookies-accepted');
+    rejectAll() {
+      this.setCookie('');
+    },
+
+    handleAcceptedPreferenceAll() {
+      this.cookieValue.preferenceCookies = true;
+      document.body.classList.remove(this.bodyPreferenceRejectedClass);
+      document.body.classList.add(this.bodyPreferenceAcceptedClass);
+      this.handleAcceptedPreference();
+      this.$emit('preferencecookiesaccepted');
+      const event = new CustomEvent('preferencecookiesaccepted');
       document.body.dispatchEvent(event);
     },
 
-    handleAccepted() {
-      // Extend this to implement your own logic when cookies are accepted
+    handleAcceptedPreference() {
+      // Extend this to implement your own logic
+      // when preference cookies are accepted
+    },
+
+    handleAcceptedStatisticAll() {
+      this.cookieValue.statisticCookies = true;
+      document.body.classList.remove(this.bodyStatisticRejectedClass);
+      document.body.classList.add(this.bodyStatisticAcceptedClass);
+      this.handleAcceptedStatistic();
+      this.$emit('statisticcookiesaccepted');
+      const event = new CustomEvent('statisticcookiesaccepted');
+      document.body.dispatchEvent(event);
+    },
+
+    handleAcceptedStatistic() {
+      // Extend this to implement your own logic
+      // when statistic cookies are accepted
+    },
+
+    handleAcceptedMarketingAll() {
+      this.cookieValue.marketingCookies = true;
+      document.body.classList.remove(this.bodyMarketingRejectedClass);
+      document.body.classList.add(this.bodyMarketingAcceptedClass);
+      this.handleAcceptedMarketing();
+      this.$emit('marketingcookiesaccepted');
+      const event = new CustomEvent('marketingcookiesaccepted');
+      document.body.dispatchEvent(event);
+    },
+
+    handleAcceptedMarketing() {
+      // Extend this to implement your own logic
+      // when marketing cookies are accepted
     },
 
     userRejects() {
@@ -117,33 +277,80 @@ export default {
       this.hideDialog();
     },
 
-    handleRejectedAll() {
-      document.body.classList.remove(this.bodyAcceptedClass);
-      document.body.classList.add(this.bodyRejectedClass);
-      this.handleRejected();
-      this.$emit('reject');
-      const event = new CustomEvent('cookies-accepted');
+    handleRejectedPreferenceAll() {
+      this.cookieValue.preferenceCookies = false;
+      document.body.classList.remove(this.bodyPreferenceAcceptedClass);
+      document.body.classList.add(this.bodyPreferenceRejectedClass);
+      this.handleRejectedPreference();
+      this.$emit('preferencecookiesrejected');
+      const event = new CustomEvent('preferencecookiesaccepted');
       document.body.dispatchEvent(event);
     },
 
-    handleRejected() {
-      // Extend this to implement your own logic when cookies are rejected
+    handleRejectedPreference() {
+      // Extend this to implement your own logic
+      // when preference cookies are rejected
     },
 
-    setCookie(value) {
-      Cookies.set(this.cookieName, value);
+    handleRejectedStatisticAll() {
+      this.cookieValue.statisticCookies = false;
+      document.body.classList.remove(this.bodyStatisticAcceptedClass);
+      document.body.classList.add(this.bodyStatisticRejectedClass);
+      this.handleRejectedStatistic();
+      this.$emit('statisticcookiesrejected');
+      const event = new CustomEvent('statisticcookiesaccepted');
+      document.body.dispatchEvent(event);
+    },
+
+    handleRejectedStatistic() {
+      // Extend this to implement your own logic
+      // when statistic cookies are rejected
+    },
+
+    handleRejectedMarketingAll() {
+      this.cookieValue.marketingCookies = false;
+      document.body.classList.remove(this.bodyMarketingAcceptedClass);
+      document.body.classList.add(this.bodyMarketingRejectedClass);
+      this.handleRejectedMarketing();
+      this.$emit('marketingcookiesrejected');
+      const event = new CustomEvent('marketingcookiesaccepted');
+      document.body.dispatchEvent(event);
+    },
+
+    handleRejectedMarketing() {
+      // Extend this to implement your own logic
+      // when marketing cookies are rejected
+    },
+
+    setCookie() {
+      Cookies.set(this.cookieName, JSON.stringify(this.cookieValue));
+      this.cookieSet = true;
     },
 
     getCookie() {
-      return Cookies.get(this.cookieName);
+      let value = Cookies.get(this.cookieName);
+
+      try {
+        value = JSON.parse(value);
+        this.cookieSet = true;
+        this.cookieValue.preferenceCookies = value.preferenceCookies;
+        this.cookieValue.statisticCookies = value.statisticCookies;
+        this.cookieValue.marketingCookies = value.marketingCookies;
+      } catch (e) {
+        this.cookieSet = false;
+      }
+
+      return this.cookieValue;
     },
 
     removeCookie() {
       Cookies.remove(this.cookieName);
     },
 
-    hasAccepted() {
-      return Cookies.get(this.cookieName) === 'allow';
+    saveOptions() {
+      this.handleChoices();
+      this.setCookie();
+      this.hideDialog();
     },
 
     showDialog() {
